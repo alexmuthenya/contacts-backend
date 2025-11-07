129aaa9b6f17d6697a4ab17f474ad207d47fe3ef
Alex <alexmuthenya100@gmail.com>
2025-11-07 11:07:50 +0300

    Add complete contacts CRUD controllers

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,27 +1,63 @@
+import mongoose from "mongoose";
+import Contact from "../models/contactModel.js";
 
-export async function getContacts(req, res){
-    return res.status(200).json({message:"Get all contacts" })
+export async function getContacts(req, res) {
+  const contacts = await Contact.find();
+  return res.status(200).json(contacts);
 }
 
-export async function createContact(req, res){
-    console.log(req.body);
-    const {name, email, phoneNumber} = req.body
-    if(!name || !email || !phoneNumber ){
-        res.status(400)
-        throw new Error("All fields are mandatory")
+export async function createContact(req, res) {
+  const { name, email, phoneNumber } = req.body;
+  if (!name || !email || !phoneNumber) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
 
-    }
-    
-    return res.status(201).json({message:"Contact created" })
+  const contact = await Contact.create({ name, email, phoneNumber });
+
+  return res.status(201).json(contact);
 }
 
-export async function getContact(req, res){
-    return res.status(200).json({message:`Get contact for ${req.params.id} ` })
+export async function getContact(req, res) {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid contact ID" });
+  }
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    return res.status(404).json({ error: "Contact not found" });
+  }
+  return res.status(200).json(contact);
 }
 
-export async function updateContact(req, res){
-    return res.status(200).json({message:`Get contact for ${req.params.id} `})
+export async function updateContact(req, res) {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid contact ID" });
+  }
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    return res.status(404).json({ error: "Contact ID not found" });
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  return res.status(200).json(updatedContact);
 }
-export async function deleteContact(req, res){
-    return res.status(200).json({message:`Delete contact for ${req.params.id} `})
+
+export async function deleteContact(req, res) {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid contact ID" });
+  }
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    return res.status(404).json({ error: "Contact ID not found" });
+  }
+
+  await Contact.findByIdAndDelete(id);
+  return res.status(200).json(contact);
 }
